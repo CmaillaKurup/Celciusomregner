a46a307d7dd7e933e6c1dcd650b451ad54258fdf
Camilla Kurup Svendsen (cami670v) <cami670v@zbc.dk>
2020-08-03 13:34:04 +0200

    Add project files.

---
+++ Celciusomregner/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Celciusomregner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double c = double.Parse(Console.ReadLine());
+            double a = c * 0.8;
+            double b = c * 1.8 + 32;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
